9f8729f3cf649884713024d092b5fe22ba43d663
MaciekDavv <dmaciek85@gmail.com>
2025-02-06 19:51:14 +0100

    all test in tests.cpp working corektly all asertion pass

--- homework/schedule/schedule.hpp
+++ homework/schedule/schedule.hpp
@@ -3,8 +3,26 @@
 #include <chrono>
 #include <thread>
 #include <functional>
+#include <optional>
+#include <string>
 
-auto schedule(std::function<void()> func,  std::chrono::seconds time) {
+auto schedule(std::function<void()> func,
+              std::chrono::seconds time) {
     std::this_thread::sleep_for(time);
     func();
+}
+
+auto schedule(std::function<void(int)> func,
+              std::chrono::seconds time,
+              int notUse) {
+    std::this_thread::sleep_for(time);
+    func(notUse);
+}
+
+auto schedule(std::function<void(std::string, double)> func,
+                                 std::chrono::seconds time,
+                                 std::string notUseString,
+                                 double notUseDouble) {
+    std::this_thread::sleep_for(time);
+    func(notUseString, notUseDouble);
 }
